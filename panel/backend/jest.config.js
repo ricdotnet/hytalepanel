e69dd3b8313a50379b13226e1c60245677438086
Vadko <vpoglid@gmail.com>
2026-01-19 20:48:31 +0200

    fix checks

--- panel/backend/jest.config.js
+++ panel/backend/jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   transform: {
